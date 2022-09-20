a303934bf8d1565a5b4b97c7bf34a8f312a9c9de
irekkkarimov <irekkkarimov@mail.ru>
2022-09-20 21:26:07 +0300

    mmm

--- pidoras/Program.cs
+++ pidoras/Program.cs
@@ -4,7 +4,57 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
-        Console.WriteLine("Test");
+        double x = double.Parse(Console.ReadLine());
+        // double s = 0;
+        // int i = 10;
+        // double p = 1;
+        // while (i > 0)
+        // {
+        //     s = s + i * p;
+        //     p = p * x;
+        //     i--;
+        // }
+        // Console.WriteLine(s);
+
+        // int i = 1;
+        // double s = 0;
+        // double p = 1;
+        //
+        // while (i < 11)
+        // {
+        //     p = p * x;
+        //     s = s + Math.Cos(x);
+        //     i++;
+        // }
+        // Console.WriteLine(s);
+
+        /*int k;
+        k = int.Parse(Console.ReadLine());
+        double s = 0;
+        double p = 1;
+        int i = 0;
+
+        while (i < k)
+        {
+            s = s + p * Math.Sin(x);
+            p = p * Math.Sin(x);
+            i++;
+        }
+        Console.WriteLine(s);*/
+
+        int i = 99;
+        double s = 0;
+        double p = 0;
+        double n = 0;
+        
+        while (i > 0)
+        {
+            p = n + Math.Sqrt(i);
+            n = i;
+            s = s + p;
+
+            i = i - 3;
+        }
+        Console.WriteLine(s);
     }
 }
