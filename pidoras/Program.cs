0c601d9382b052b3af97e9628d781543c03c618d
irekkkarimov <irekkkarimov@mail.ru>
2022-09-26 12:57:05 +0300

    fixed methods

--- pidoras/Program.cs
+++ pidoras/Program.cs
@@ -4,76 +4,80 @@ public class Program
 {
     static void Main(string[] args)
     {
-        static void eq_with_expon()
-        {
-            double x = double.Parse(Console.ReadLine());
-            double s = 0;
-            int i = 10;
-            double p = 1;
-            while (i > 0)
-            {
-                s = s + i * p;
-                p = p * x;
-                i--;
-            }
-
-            Console.WriteLine(s);
-        }
-
-
-        static void cos_with_expon()
-        {
-            double x = double.Parse(Console.ReadLine());
-            int i = 1;
-            double s = 0;
-            double p = 1;
-
-            while (i < 11)
-            {
-                p = p * x;
-                s = s + Math.Cos(x);
-                i++;
-            }
-
-            Console.WriteLine(s);
-        }
-
-
-        static void sin()
-        {
-            double x = double.Parse(Console.ReadLine());
-            int k;
-            k = int.Parse(Console.ReadLine());
-            double s = 0;
-            double p = 1;
-            int i = 0;
-
-            while (i < k)
-            {
-                s = s + p * Math.Sin(x);
-                p = p * Math.Sin(x);
-                i++;
-            }
-
-            Console.WriteLine(s);
-        }
-
-
-        static void Sqrts()
-        {
-            int i = 99;
-            double s = 0;
-            double p = 0;
-            double n = 0;
-
-            while (i > 0)
-            {
-                p = Math.Sqrt(p + i);
-                s = s + p;
-                i = i - 3;
-            }
-
-            Console.WriteLine(s);
-        }
+        // Call a method down below
+        
     }
+    
+        static double eq_with_expon()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double s = 0;
+        int i = 10;
+        double p = 1;
+        while (i > 0)
+        {
+            s = s + i * p;
+            p = p * x;
+            i--;
+        }
+
+        return s;
+    }
+
+
+    static double cos_with_expon()
+    {
+        double x = double.Parse(Console.ReadLine());
+        int i = 1;
+        double s = 0;
+        double p = 1;
+
+        while (i < 11)
+        {
+            p = p * x;
+            s = s + Math.Cos(x);
+            i++;
+        }
+
+        return s;
+    }
+
+
+    static double sin()
+    {
+        double x = double.Parse(Console.ReadLine());
+        int k;
+        k = int.Parse(Console.ReadLine());
+        double s = 0;
+        double p = 1;
+        int i = 0;
+
+        while (i < k)
+        {
+            s = s + p * Math.Sin(x);
+            p = p * Math.Sin(x);
+            i++;
+        }
+
+        return s;
+    }
+
+
+    static double Sqrts()
+    {
+        int i = 99;
+        double s = 0;
+        double p = 0;
+        double n = 0;
+
+        while (i > 0)
+        {
+            p = Math.Sqrt(p + i);
+            s = s + p;
+            i = i - 3;
+        }
+
+        return s;
+    }
+    
 }
